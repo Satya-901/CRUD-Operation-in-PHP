c0844b1c4a39fc78a2ce9385e989955fa47b536e
Satya-901 <114366791+Satya-901@users.noreply.github.com>
2022-11-28 22:10:02 +0530

    started to code

---
+++ index.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+    <!-- ===CDN link for bootsrep === -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <!-- ===FOnr link=== -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap" rel="stylesheet">
+<!-- ===Custom CSS=== -->
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1 class="text-center bg-dark text-light">Student Details</h1>
+    <!-- ===JS CDN link=== -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
